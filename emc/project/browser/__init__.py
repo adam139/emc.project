b585c1d1e715208d8f4553d79576547520ea3520
adam139 <yuejun.tang@gmail.com>
2016-01-30 09:48:42 +0800

    fix plone domain translation

---
+++ emc/project/browser/__init__.py
@@ -0,0 +1,13 @@
+from Products.CMFPlone import PloneMessageFactory as _
+#_ = MessageFactory('plone')
+dummy = _("FangAn")
+dummy = _("ChuYang")
+dummy = _("ChuYang1")
+dummy = _("ChuYang2")
+dummy = _("ShiYang")
+dummy = _("ShiYang1")
+dummy = _("ShiYang2")
+dummy = _("ZhengYang")
+dummy = _("ZhengYang1")
+dummy = _("ZhengYang2")
+dummy = _("DingXing")
--- emc/project/rebuild_plone_i18n.sh
+++ emc/project/rebuild_plone_i18n.sh
@@ -3,7 +3,7 @@ PRODUCTNAME='plone'
 I18NDOMAIN=$PRODUCTNAME
 
 # Synchronise the .pot with the templates.
-../../../../bin/i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTNAME}-manual.pot --create ${I18NDOMAIN} .
+# ../../../../bin/i18ndude --merge  locales/${PRODUCTNAME}.pot  locales/${PRODUCTNAME}-manual.pot 
 
 # Synchronise the resulting .pot with the .po files
 ../../../../bin/i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/zh_CN/LC_MESSAGES/${PRODUCTNAME}.po
