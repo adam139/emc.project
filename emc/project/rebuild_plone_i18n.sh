f41d0e1c3972f3844f8b71b0f16804ee9e7b5989
adam139 <yuejun.tang@gmail.com>
2018-12-25 11:26:18 +0800

    add ProjectManager role and translation

--- emc/project/rebuild_plone_i18n.sh
+++ emc/project/rebuild_plone_i18n.sh
@@ -3,7 +3,7 @@ PRODUCTNAME='plone'
 I18NDOMAIN=$PRODUCTNAME
 
 # Synchronise the .pot with the templates.
-# ../../../../bin/i18ndude --merge  locales/${PRODUCTNAME}.pot  locales/${PRODUCTNAME}-manual.pot 
+../../../../bin/i18ndude merge --pot  locales/${PRODUCTNAME}.pot  --merge locales/${PRODUCTNAME}-manual.pot 
 
 # Synchronise the resulting .pot with the .po files
 ../../../../bin/i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/zh_CN/LC_MESSAGES/${PRODUCTNAME}.po
