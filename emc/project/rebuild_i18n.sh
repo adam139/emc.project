c16f4db13dee530776445d1c174e0645a44f6230
adam139 <yuejun.tang@gmail.com>
2020-10-19 09:51:29 +0800

    update translations

--- emc/project/rebuild_i18n.sh
+++ emc/project/rebuild_i18n.sh
@@ -3,7 +3,7 @@ PRODUCTNAME='emc.project'
 I18NDOMAIN=$PRODUCTNAME
 
 # Synchronise the .pot with the templates.
-../../../../bin/i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTNAME}-manual.pot --create ${I18NDOMAIN} .
+i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --merge locales/${PRODUCTNAME}-manual.pot --create ${I18NDOMAIN} .
 
 # Synchronise the resulting .pot with the .po files
-../../../../bin/i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/zh_CN/LC_MESSAGES/${PRODUCTNAME}.po
+i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/zh_CN/LC_MESSAGES/${PRODUCTNAME}.po
